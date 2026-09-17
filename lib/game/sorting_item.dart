170e8987322c68c3e20dd2f9021723b4dfccf863
dharmesh-eww <dharmesh.gondaliya@excellentwebworld.in>
2026-09-17 18:35:31 +0530

    Refactor sorting item identity

--- lib/game/sorting_item.dart
+++ lib/game/sorting_item.dart
@@ -1,13 +1,23 @@
 class SortingItem {
   const SortingItem({
+    required this.itemId,
     required this.productId,
     required this.asset,
     required this.stackIndex,
     required this.shelfIndex,
   });
 
+  final int itemId;
   final int productId;
   final String asset;
   final int stackIndex;
   final int shelfIndex;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SortingItem && other.itemId == itemId;
+
+  @override
+  int get hashCode => itemId.hashCode;
 }
