edc34fed84afb34b688476e5e24547fe27e05edc
dharmesh-eww <dharmesh.gondaliya@excellentwebworld.in>
2026-09-17 16:43:50 +0530

    Add generated level data model

---
+++ lib/game/level_data.dart
@@ -0,0 +1,25 @@
+import 'sorting_item.dart';
+
+class SortingLevel {
+  const SortingLevel({
+    required this.levelNumber,
+    required this.difficulty,
+    required this.itemTypes,
+    required this.shelfCount,
+    required this.maxStackDepth,
+    required this.emptySlots,
+    required this.complexity,
+    required this.items,
+  });
+
+  final int levelNumber;
+  final double difficulty;
+  final int itemTypes;
+  final int shelfCount;
+  final int maxStackDepth;
+  final int emptySlots;
+  final double complexity;
+  final List<SortingItem> items;
+
+  int get totalItems => items.length;
+}
